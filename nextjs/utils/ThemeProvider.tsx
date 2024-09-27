f10d35e632331c1ea91b4e59028c7dcad86111f0
QuarkyQuo <sudheerkumarkota10@gmail.com>
2024-09-27 23:04:19 +0530

    post merge fixes

--- nextjs/utils/ThemeProvider.tsx
+++ nextjs/utils/ThemeProvider.tsx
@@ -1,5 +1,8 @@
 "use client";
-import {ThemeProvider as NextThemesProvider} from "next-themes";
-export default function ThemeProvider({children, ...props}) {
+import { ReactNode } from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+
+export default function ThemeProvider({ children, ...props }: any) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
