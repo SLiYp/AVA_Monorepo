fe58d47753fe8a112e2c999bcb3de2f05f63c143
QuarkyQuo <sudheerkumarkota10@gmail.com>
2024-09-27 20:09:35 +0530

    revert previous rag implimentations

--- flask/rag.py
+++ flask/rag.py
@@ -12,7 +12,6 @@ from langchain.schema import StrOutputParser
 from langchain_community.document_loaders import CSVLoader
 from langchain_openai import OpenAIEmbeddings, OpenAI, ChatOpenAI
 import chromadb
-from text_to_chunks import load_text_from_file,chunk_pdf_text
 
 # Load environment variables
 load_dotenv()
@@ -24,21 +23,13 @@ embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
 # Initialize ChromaDB client and collection
 chroma_client = chromadb.PersistentClient(path="./chromadb/")
 
-#load and preprocess custom embeeding data
-
-def pdfDataLoader(filePath):
-    pdf_text= load_text_from_file(filePath)
-    chunked_structure = chunk_pdf_text(pdf_text, max_chunk_size=1000)
-    return chunked_structure;
-    
-    
 
 # Define a retriever class using ChromaDB directly
 class ChromaRetriever:
     def __init__(self):
         self.self=self
         
-    def embed(self,documentRef,doc_type="text"):
+    def embed(self,documentRef):
         # collection = chroma_client.get_or_create_collection(name="primary_collection")
         collections = chroma_client.list_collections()
         collection_exists = False
@@ -49,44 +40,24 @@ class ChromaRetriever:
 
         if collection_exists:
             collection= chroma_client.get_collection(documentRef["id"])
-        else:
+        if(collection_exists !=True):
+            # Create embeddings from CSV documents and upsert them into ChromaDB
             collection = chroma_client.create_collection(documentRef["id"])
-        
-        print(collection)
-        # Create embeddings from CSV documents and upsert them into ChromaDB
-        if(doc_type == "csv"):
             for i, doc in enumerate(documentRef["documents"]):
-                embedding = embeddings.embed_query(doc.page_content | doc)  # Generate embedding for document content
+                embedding = embeddings.embed_query(doc.page_content)  # Generate embedding for document content
                 # print(embedding)
                 collection.upsert(
                     embeddings=[embedding],
-                    documents=[doc.page_content | doc],  # Use document content
+                    documents=[doc.page_content],  # Use document content
                     ids=[str(i)]  # Use document index as ID
                 )
-        else:
-            for doc in documentRef["documents"]:
-                print(doc)
-                for sub_doc in doc['sub_docs']:
-                    for i, chunk in enumerate(sub_doc['chunks']):
-                        embedding = embeddings.embed_query(chunk)  # Generate embedding for document content
-                        print(chunk)
-                        collection.upsert(
-                            embeddings=[embedding],
-                            documents=[chunk],  # Use document content
-                            ids=[f'{chunk.metadata.doc_id}']  # Use document index as ID
-                        )
 
-
-    def retrieve(self, query, top_k=3,collection_list="all"):
+    def retrieve(self, query, top_k=3):
         query_embedding = embeddings.embed_query(query)  # Ensure the same embeddings instance
         all_results = []
 
         # Get all collections
-        if(collection_list != "all"):
-            # isinstance(doc, list) for doc in retrieved_docs
-            collections = [chroma_client.get_collection(name) for name in collection_list]
-        else:
-            collections = chroma_client.list_collections()
+        collections = chroma_client.list_collections()
 
         # Loop through each collection and query
         for collection in collections:
@@ -103,21 +74,18 @@ class ChromaRetriever:
 
         # Return top_k results across all collections
         if all_results:
-            print(all_results)
             return [doc for doc, distance in all_results[:top_k]]
         else:
             return ["No results found."]
 
 # Load CSV data using CSVLoader
-# loader = CSVLoader(file_path='./custom_data/DataSet.csv')
-# documents = loader.load()  # This returns a list of Documents with content
-documents=pdfDataLoader("./custom_data/The-Science-of-Human-Nature-A-Psychology-for-Beginners.txt")
-# print(documents)
-docRef={"id":"collection_2","documents":documents}
-print([value for i,value in enumerate(docRef["documents"])])
+loader = CSVLoader(file_path='./custom_data/DataSet.csv')
+documents = loader.load()  # This returns a list of Documents with content
+docRef={"id":"collection_1","documents":documents}
+# print(docRef["documents"])
 retriever=ChromaRetriever()
-retriever.embed(docRef)
-retriever.retrieve("Which are unwise and mistaken,Republicans or Democrats?",collection_list=["collection_2"])
+# retriever.embed(docRef)
+
 # Initialize LLM
 model = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
 llm = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
@@ -128,7 +96,7 @@ class SimpleRAG:
         self.retriever = retriever
 
     def generate(self, query):
-        retrieved_docs = self.retriever.retrieve(query,collection_list=["collection_2"])
+        retrieved_docs = self.retriever.retrieve(query)
         # print(retrieved_docs)
         if all(isinstance(doc, list) for doc in retrieved_docs):
             retrieved_docs = [item for sublist in retrieved_docs for item in sublist]
@@ -158,31 +126,23 @@ def get_session_history(session_id: str) -> FileChatMessageHistory:
 # Define prompt template for conversation
 prompt = ChatPromptTemplate.from_messages(
     [   
-("human", f'''You are a friend designed to enhance user interactions. You will do quick chats with chat lenghts not more than 20-30 words. In general you will respond with in 10 - 15 words. 
+("human", f'''You are a highly effective assistant designed to enhance user interactions by fetching contextual data to provide relevant responses. Your goal is to analyze user prompts and, whenever applicable, incorporate context from the database to generate insightful answers that align with user inquiries. 
 Your task is to generate a response based on the user prompt and the relevant context retrieved from the database. The response should be adaptable and generic enough to address a wide range of queries; however, if there's a direct correlation between the user prompt and the context, you should seamlessly integrate that information into your answer.
 
-secondary obligations:
-    - As a psychologist, your role is to follow evidence-based methods to help you navigate mental wellness. 
-    - We may go through assessments, frameworks, and specific steps that are used in psychological practice. 
-    - you are here to guide user, but you won’t always offer immediate support — some of this process involves self-reflection and understanding the root of the issues.
-    - Help user start by exploring what you’re experiencing. Ask a few questions to better understand the situation before deciding the appropriate next steps.
+**Response Format:** Please format your answers using Markdown. This includes:
+- Using `**bold**` for emphasis.
+- Using `*italic*` for emphasis or differentiation.
+- Using `-` or `*` for lists.
+- you are not a programmer and you don't write code.
+- Providing links or resources if applicable.
 
-Response Format: 
-    - list of Markdown strings with less than 20 words in each string.
-    - Providing links or resources if applicable.
+**Tone:** Keep it casual and friendly, like chatting with a friend. Avoid lengthy answers and don't use unnecessary introductory or concluding phrases like "I hear you," "I understand you," "I see you are referring to," or "From what I understand."
 
-Response style: 
-    - Keep it very casual and friendly, like chatting with a friend.
-    - You are not a scholor to know everything. Skip if you are challenged academically or professionally. 
-    - Avoid lengthy answers. Don't use unnecessary phrases like "I hear you," "I understand you," "I see you are referring to," or "From what I understand." and more like these.
-Restrictions:
-    - If the human asks you about the context, don't reveal the context.
-    - You do not facilitate conversations involving sensitive matters.
-    
+- If the human asks you about the context, don't reveal the context.
+- If the human asks about a previous question, give it.
 Keep in mind that if the context does not pertain to the user’s prompt, it’s acceptable for you to provide a more generalized answer without incorporating any contextual information. Your responses should aim to be clear, concise, and informative, enhancing the user’s experience while ensuring relevance to their request.'''),
 MessagesPlaceholder(variable_name="chat_history"),
 ("system","Here are the details you need to refer for generating your response. You can ignore if reference doesn't suit the situation. Context_from_Database:{context}"),
-("system","Don’t justify your answers. Don’t give information not mentioned in the CONTEXT INFORMATION."),
 ("human", "{input}")
 
     ]
@@ -204,6 +164,6 @@ def process_llm_prompt(session_id: str, prompt_details: dict):
 
 # Example usage
 session_id = "user_session_1"
-prompt_details = {"prompt": "how can mental health for adults impact?"}
+prompt_details = {"prompt": "what is sun"}
 response = process_llm_prompt(session_id, prompt_details)
 print("Conversation Response:", response)
